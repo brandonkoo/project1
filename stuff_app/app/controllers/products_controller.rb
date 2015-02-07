77d37c74c5f3cdbee63baaf1b40275a51ee3a45a
brandonkoo <brandonkoo555@gmail.com>
2015-02-07 17:03:13 -0500

    updated products edit function

--- stuff_app/app/controllers/products_controller.rb
+++ stuff_app/app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 require 'byebug'
 class ProductsController < ApplicationController
+  before_action :set_products, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
   end
@@ -16,19 +17,51 @@ class ProductsController < ApplicationController
 
     @product = Product.new(product_params) #(params[:prod_name, :description, :price])
     if @product.save
+      redirect_to "/products"
+
+      else
+        format.html { render :new }
+        format.json { render json: @products.errors, status: :unprocessable_entity }
+
+      end
 
       #binding.byebug
-      redirect_to "/products"
-    else
-      render :new
+  end
+
+  def update
+    respond_to do |format|
+      if @products.update(product_params)
+        format.html { redirect_to products_url, notice: 'products was successfully updated.' }
+
+      else
+        format.html { render :edit }
+
+      end
     end
   end
 
+  def edit
+    @products = Product.find(params[:id])
+
+  end
+
+  def destroy
+    @products.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'products was successfully destroyed.' }
+  end
+  end
+
+
+
+  private
 
   def product_params
     params.require(:product).permit(:prod_name, :description, :price)
   end
-
+  def set_products
+      @products = Product.find(params[:id])
+    end
 
 
 end
--- stuff_app/config/routes.rb
+++ stuff_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :customers
-  resources :products
-  resources :orders
+  resources :customers, :products, :orders
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
